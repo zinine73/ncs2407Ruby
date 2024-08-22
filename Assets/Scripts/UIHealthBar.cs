1dc0e39c2d09b259882c535ca818b9c04804ad39
zinine <zinine@naver.com>
2024-08-22 15:40:05 +0900

    HUD

---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public static UIHealthBar instance { get; private set; }
+
+    public UnityEngine.UI.Image mask;
+    float originalSize;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;    
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
